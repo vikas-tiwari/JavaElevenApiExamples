dbb473949cb6472bd933ee6f475ab6d2dac7b1a3
Vikas Tiwari <vikas.tiwari990@gmail.com>
2020-04-05 18:16:49 +0530

    updated lambda code

--- src/Lambda/LambdaDemo.java
+++ src/Lambda/LambdaDemo.java
@@ -2,6 +2,7 @@ package Lambda;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.*;
 
 public class LambdaDemo {
     public static void main(String args[]) {
@@ -10,12 +11,56 @@ public class LambdaDemo {
         FileFilter filter = new FileFilter() {
             @Override
             public boolean accept(File file) {
-                return false;
+                return file.getName().endsWith(".java");
             }
         };
 
         //With lambda Expression
         FileFilter fileFilter = (File file) -> file.getName().endsWith(".java");
+
+
+        File dir = new File("/home/vikas/IdeaProjects/JavaElevenApiExamples/src/Lambda");
+        File[] files = dir.listFiles(fileFilter);
+        for(File file : files) {
+            System.out.println(file);
+        }
+
+
+        //Anonymous class
+        Runnable runnableClass = new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("Inside runnable anonymous class");
+            }
+        };
+
+        //Lambda expression
+        Runnable runnableLambda = () -> {
+            for (int i=0;i<3;i++) {
+                System.out.println("Inside lambda expression");
+            }
+        };
+
+        Thread thread = new Thread(runnableLambda);
+        thread.start();
+
+        //Anonymous class
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String s, String t1) {
+                return s.compareTo(t1);
+            }
+        };
+
+        //Lambda
+        Comparator<String> stringComparator = (s1, s2) -> s1.compareTo(s2);
+
+        List<String> list = Arrays.asList("India"," USA", "Canada");
+        Collections.sort(list, stringComparator);
+
+        for(String s: list) {
+            System.out.println(s);
+        }
 
 
     }
